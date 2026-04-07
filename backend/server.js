a1131f1e233169c91cb728af3d2123698f918d01
Franklin Beltre <fabeltre@gmail.com>
2026-04-07 10:02:48 -0400

    Sirve frontend desde Express y usa rutas relativas en la API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express        = require('express');
 const session        = require('express-session');
 const cors           = require('cors');
+const path           = require('path');
 const usuariosRouter = require('./routes/usuarios');
 
 const app  = express();
@@ -22,6 +23,8 @@ app.use(session({
 }));
 
 app.use('/api', usuariosRouter);
+
+app.use(express.static(path.join(__dirname, '..')));
 
 app.listen(PORT, () => {
     console.log('Servidor ISW306 Grupo 1 activo en http://localhost:' + PORT);
